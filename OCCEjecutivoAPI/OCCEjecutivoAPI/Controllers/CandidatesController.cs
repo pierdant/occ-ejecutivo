1ef0b42f2b3938ee294df86653f62cf39412b9ff
pierdant <eduardop@occ.com.mx>
2016-09-07 17:09:34 -0500

    haciendo un commit con las primeras pruebas unitarias

--- OCCEjecutivoAPI/OCCEjecutivoAPI/Controllers/CandidatesController.cs
+++ OCCEjecutivoAPI/OCCEjecutivoAPI/Controllers/CandidatesController.cs
@@ -6,10 +6,14 @@ using System.Net.Http;
 using System.Web.Http;
 using OCCEjecutivoAPI.Models;
 
+using System.Diagnostics;
+
 namespace OCCEjecutivoAPI.Controllers
 {
     public class CandidatesController : ApiController
     {
+        //todo: ver con Karina si podemos o como deberiamos hacer esto para que UTest siempre jale
+
 
         Candidate[] allThePeople = new Candidate[]
         {
@@ -31,11 +35,19 @@ namespace OCCEjecutivoAPI.Controllers
 
         public IHttpActionResult GetCandidateData(string id)
         {
-            var aPerson = allThePeople.First((p) => p.Id == id);
-            if (aPerson == null)
+            try
+            {
+                var aPerson = allThePeople.First((p) => p.Id == id);
+                if (aPerson == null)
+                    return NotFound();
+                else
+                    return Ok(aPerson);
+
+            }
+            catch (Exception)
+            {
                 return NotFound();
-            else
-                return Ok(aPerson);
+            }
         }
     }
 }
