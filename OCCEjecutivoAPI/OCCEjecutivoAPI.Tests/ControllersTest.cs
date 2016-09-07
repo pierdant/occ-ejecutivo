4f33735f9f59e9ecd7126b980e7e438312856257
pierdant <eduardop@occ.com.mx>
2016-09-07 17:11:41 -0500

    este es el Unit test que queria agregar
     Please enter the commit message for your changes. Lines starting

---
+++ OCCEjecutivoAPI/OCCEjecutivoAPI.Tests/ControllersTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OCCEjecutivoAPI.Controllers;
+using OCCEjecutivoAPI.Models;
+
+namespace OCCEjecutivoAPI.Tests
+{
+    [TestClass]
+    public class ControllersTest
+    {
+        [TestMethod]
+        public void GetCandidate_ShouldBeThere()
+        {
+            var controller = new CandidatesController();
+            var result = controller.GetCandidateData("2") as OkNegotiatedContentResult<Candidate>;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Gonzalo", result.Content.FirstName);
+
+
+            result = controller.GetCandidateData("3") as OkNegotiatedContentResult<Candidate>;
+            Assert.IsNull(result);
+        }
+
+    }
+}
+
