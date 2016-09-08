4231e9de315a4b881847d222242756a439738aea
pierdant <eduardop@occ.com.mx>
2016-09-08 14:48:10 -0500

    Listos los cambios en todo el controlador de Candidatos (CRUD) con todo y tests

--- OCCEjecutivoAPI/OCCEjecutivoAPI/Controllers/UsersController.cs
+++ OCCEjecutivoAPI/OCCEjecutivoAPI/Controllers/UsersController.cs
@@ -4,10 +4,63 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using OCCEjecutivoAPI.Models;
 
 namespace OCCEjecutivoAPI.Controllers
 {
     public class UsersController : ApiController
     {
+        //Get /api/Users/#
+        public HttpResponseMessage GetUserData(string id)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        // Post /api/Users/{#} json candidate object
+        public HttpResponseMessage SaveCandidateData(User user, string id = "0")
+        {
+            bool existingObject = false;
+
+            #region FieldParamValidation
+
+            if (id.CompareTo("0") != 0)
+            {
+                existingObject = true;
+                if (String.IsNullOrEmpty(user.id))
+                    return Request.CreateErrorResponse(HttpStatusCode.Conflict, "null or empty object_id does not matches instance_id");
+                if (!id.Equals(user.id))
+                    return Request.CreateErrorResponse(HttpStatusCode.Conflict, "instance_Id different from object_id");
+            }
+
+            if (String.IsNullOrEmpty(user.Email))
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "null or empty email");
+            #endregion
+
+            try
+            {
+                DataContext cn = new DataContext();
+
+
+                if (existingObject)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.Created);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
     }
 }
